d8a3dcfbd4044495032108996a4748df3686dbdb
Laura Boemo <71987461+LauraBoemo@users.noreply.github.com>
2022-01-17 00:11:04 -0300

    Day#18
    Array/Objects

---
+++ 5_array.js/3_arrrayWithObjects.js
@@ -0,0 +1,13 @@
+// Simulando um Objeto a partir de um Array...
+const quaseArray = { 0: 'Rafael', 1: 'Ana', 2: 'Bia'}
+console.log(quaseArray) // Continua sendo um objeto e não um array
+
+Object.defineProperty(quaseArray, 'toString', {
+    value: function() { return Object.values(this) },
+    enumerable: false
+})
+
+console.log(quaseArray[0])
+
+const meuArray = ['Rafael', 'Ana', 'Bia']
+console.log(quaseArray.toString(), meuArray)

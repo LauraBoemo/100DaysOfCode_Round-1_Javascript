6b56d457759dd8fc8e144bafe69c76beb0ea6517
Laura Boemo <71987461+LauraBoemo@users.noreply.github.com>
2022-01-19 08:58:32 -0300

    Day#20

--- 5_array.js/8_imperativoEDeclarativo.js
+++ 5_array.js/8_imperativoEDeclarativo.js
@@ -13,3 +13,5 @@ const getNota = aluno => aluno.nota
 const soma = (total, atual) => total + atual
 const total2 = alunos.map(getNota).reduce(soma)
 console.log(total2 / alunos.length)
+
+// Dia vazio

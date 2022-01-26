76aec6e5aa408ac31d4ed5076e2fb7ef742095b0
Laura Boemo <71987461+LauraBoemo@users.noreply.github.com>
2022-01-25 23:46:40 -0300

    Day#26

---
+++ 5_array.js/9_concat.js
@@ -0,0 +1,6 @@
+const filhas = ['Muriel', 'Lauren']
+const filhos = ['Fleivio', 'Murilo']
+const todos = filhas.concat(filhos, 'outro concat')
+console.log(todos, filhas, filhos)
+
+console.log(['a', 'b'].concat([1, 2], [3, 4], 5, [[6, 7]]))
